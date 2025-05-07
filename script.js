1a2752dae64dd1d7d83903a8cac47b037af5d7bb
mestakarya <mestakarya@gmail.com>
2025-05-07 15:19:10 +0000

    Save changes from Codespaces

---
+++ script.js
@@ -0,0 +1,9 @@
+// Contoh sederhana animasi GSAP saat halaman dimuat
+gsap.from(".container", {
+    duration: 1,
+    opacity: 0,
+    y: 50,
+    ease: "power2.out"
+});
+
+// Kamu bisa menambahkan animasi GSAP lainnya di sini
